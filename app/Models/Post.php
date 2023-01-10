57b739ee902faf1b8c81dc2f6d863ccd4d04d807
Eminson Mendoza <emimaster16@gmail.com>
2023-01-10 17:29:54 -0500

    error en relaciones

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Tag;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table = 'posts';
 
     // Relacion uno a muchos inversa user
     public function user()
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,14 +1,26 @@
 <x-app-layout> 
 
-    <div class="container max-w-7xl mx-auto px-2 sm: px-6 lg:px-8 ">
+    <div class="container max-w-7xl mx-auto px-2 sm: px-6 lg:px-8 py-8">
         <div class="grid grid cols-3 gap-6 ">
 
             {{-- @dump($posts->images->url) --}}
 
 
             @foreach ($posts as $post)
-                <article>
-                    {{ $post->images_id }}
+
+                {{-- {{ $post->id }} --}}
+
+                <article class="w-full h-40 bg-cover bg-center @if($loop->first) col-span-2 @endif" style="background-image: blue ">
+                    <div class="w-full h-full px-8 flex flez-col justify-center">
+                        <div>
+                            {{ $post->tags }}
+                        </div>
+                        <h1 class="text-4xl text-blue-500 leading-8 font-bold">
+                            <a href="">{{ $post->name }}</a>
+                        </h1>
+
+
+                    </div>
                 </article>
             @endforeach
 
